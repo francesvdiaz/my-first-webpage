595d4b4a3162df85586a668aac39a59a9845cba6
FrancesDiaz <115190000+francesvdiaz@users.noreply.github.com>
2023-01-31 21:52:50 -0800

    add dark mode button

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  var element = document.body;
+  element.onclick = function () {
+    element.style.backgroundColor = "purple";
+    element.style.color = "green"
+
+  }
+}
